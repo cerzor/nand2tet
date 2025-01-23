208fd81bd777c834beb7526da04c84aea5d0494f
Justin Ford <k3k3@shitonoeru.local>
2025-01-22 22:53:15 -0500

    assembler works... but it is ugly.

--- 6/assembler/Assembler.cpp
+++ 6/assembler/Assembler.cpp
@@ -16,6 +16,7 @@ inline string trim(const string& s, const char* t = whitespace)
     result.erase(result.find_last_not_of(t) + 1);
     return result;
 }
+
 string toBinary16(const string& str){
     int num = stoi(str);
     bitset<16> binary(num);
@@ -23,12 +24,21 @@ string toBinary16(const string& str){
 }
 
 string findNextMemoryByValue(const map<string, string>& addressMap){
-    int num = 100000;
-    for (const  pair<const string, string>& pair : addressMap) {
-        if (pair.second == to_string(num)) {
-            num++;
-        } else {
-            break;
+    vector<int> values;
+    for (const auto& pair : addressMap) {
+        int value = stoi(pair.second, nullptr, 2);  // Convert binary string to an integer.
+        if (value >= 16 && value < 16384) {  // We only care about values in the range [16, 16383].
+            values.push_back(value);
+        }
+    }
+
+    sort(values.begin(), values.end());
+
+    // Start searching for the next available value greater than or equal to 16.
+    int num = 16;
+    for (int value : values) {
+        if (value == num) {
+            num++;  // If the value is already taken, move to the next number.
         }
     }
     bitset<16> binary(num);
@@ -58,10 +68,18 @@ int main(int argc, char* argv[]) {
         {"M","001"},
         {"D","010"},
         {"DM","011"},
+        {"MD","011"},
         {"A","100"},
         {"AM","101"},
         {"AD","110"},
-        {"ADM","111"}
+        {"MA","101"},
+        {"DA","110"},
+        {"ADM","111"},
+        {"AMD","111"},
+        {"DAM","111"},
+        {"DMA","111"},
+        {"MAD","111"},
+        {"MDA","111"}
     };
 
     map<string, string> compTable = {
@@ -105,63 +123,119 @@ int main(int argc, char* argv[]) {
         {"JLE","110"},
         {"JMP","111"}
     };
-    //End lookup tables
 
-    //vector<pair<string, string>> addressStore; //scared of memory usage
+    map<string, string> preDef = {
+        {"SP", "0000000000000000"},
+        {"LCL", "0000000000000001"},
+        {"ARG", "0000000000000010"},
+        {"THIS", "0000000000000011"},
+        {"THAT", "0000000000000100"},
+        {"R0", "0000000000000000"},
+        {"R1", "0000000000000001"},
+        {"R2", "0000000000000010"},
+        {"R3", "0000000000000011"},
+        {"R4", "0000000000000100"},
+        {"R5", "0000000000000101"},
+        {"R6", "0000000000000110"},
+        {"R7", "0000000000000111"},
+        {"R8", "0000000000001000"},
+        {"R9", "0000000000001001"},
+        {"R10", "0000000000001010"},
+        {"R11", "0000000000001011"},
+        {"R12", "0000000000001100"},
+        {"R13", "0000000000001101"},
+        {"R14", "0000000000001110"},
+        {"R15", "0000000000001111"},
+        {"SCREEN", "0100000000000000"},
+        {"KBD", "0110000000000000"}
+    };
+    //End lookup tables
     map<string, string> addressMap; //scared of memory usage
-    string D = to_string(0000000000000000);
-    string A = to_string(0000000000000000);
-    string M = to_string(0000000000000000);
+    string D = "0000000000000000";
+    string A = "0000000000000000";
+    string M = "0000000000000000";
     string binCode = "0000000000000000";
     string comp;
     string dest;
     string jmp;
     string a;
     string line;
+    int lineCount = 0;
+    int commentLines = 0;
+    vector<string> lines;
+    vector<string> labels;
 
     while(getline(file, line)){
         string trimmedLine = trim(line);
-        if(trimmedLine.length() == 0) continue;
+        if(trimmedLine.length() == 0 || trimmedLine.substr(0,2) == "//") {
+            commentLines++;
+            continue;
+        }
+        if(trimmedLine[0] == '('){
+            labels.push_back(trimmedLine);
+        }
+        lines.push_back(trimmedLine);
+    }
 
-        if(trimmedLine.substr(0,2) != "//"){ //Check if the line is blank or a commment and ignore it.
-            if (trimmedLine[0] == '@' &&
-            (trimmedLine.substr(1).find_first_not_of("0123456789") ||
-            (trimmedLine[1] == 'R' && trimmedLine.substr(2).find_first_not_of("0123456789")))) { //A instruction by mem reference
+    for (string& label : labels){
+        string varName = label.substr(1, label.size()-2);
+        int lineIndex = find(lines.begin(), lines.end(), "(" + varName + ")") - lines.begin();
+        bitset<16> binary(lineIndex - lineCount);
+        addressMap[varName] = binary.to_string();
+        lineCount++;
+    }
+
+    for (const pair<const string, string>& pre : preDef){
+        addressMap[pre.first] = pre.second;
+    }
+    for (string& line : lines) {
+        if(line.substr(0,2) != "//" && line[0] != '('){ //Check if the line is blank or a commment and ignore it.
+            if (line[0] == '@' &&
+            (line.substr(1).find_first_not_of("0123456789") ||
+            (line[1] == 'R' && line.substr(2).find_first_not_of("0123456789")))) { //A instruction by mem reference
                     int offset = 1;
-                    if (trimmedLine[1] == 'R') {
+                    if (line[1] == 'R') {
                         offset = 2;
                     }
-                    binCode = toBinary16(trimmedLine.substr(offset));
-                    A = stoi(binCode);
-                    bool addPresent = addressMap.find("var") == addressMap.end();
+                    binCode = toBinary16(line.substr(offset));
+                    A = binCode;
+                    bool addPresent = addressMap.find(line.substr(offset)) == addressMap.end();
                     if (addPresent){
-                        addressMap[trimmedLine.substr(offset)] = stoi(binCode);
+                        addressMap[line.substr(offset)] = A;
                     }
                     cout << binCode << endl;
-            } else if (trimmedLine[0] == '@'){ //A instruction dynamic
-                string varName = trimmedLine.substr(1, trimmedLine.size() -1);
-                bool varPresent = addressMap.find("var") != addressMap.end();
-                if (varPresent){
-                    A = addressMap["varName"];
+            } else if (line[0] == '@'){ //A instruction dynamic
+                string varName = line.substr(1, line.size() - 1);
+                bool preDefPresent = preDef.find(varName) != preDef.end();
+                if (preDefPresent) {
+                    A = preDef[varName];
                 } else {
-                    A = findNextMemoryByValue(addressMap);
+                    bool varPresent = addressMap.find(varName) != addressMap.end();
+                    if (varPresent){
+                        A = addressMap[varName];
+                    } else {
+                        A = findNextMemoryByValue(addressMap);
+                        addressMap[varName] = A;
+                    }
                 }
                 cout << A << endl;
 
-            } else if(trimmedLine[0] == '('){ //L instruction
-                // cout << trimmedLine << endl;
             }  else { //C instruction
-                size_t endPos = trimmedLine.find(";");
+                size_t endPos = line.find(";");
                 if (endPos == std::string::npos) {
                     jmp = "000";
-                    endPos = trimmedLine.length();
+                    endPos = line.length();
+                    dest = destTable[trim(line.substr(0, line.find("=")))];
                 } else {
-                    jmp = jmpTable[trim(trimmedLine.substr(trimmedLine.find(";") + 1))];
+                    dest = destTable[line.substr(0, endPos + 1)];
+                    jmp = jmpTable[trim(line.substr(line.find(";") + 1))];
                 }
-                comp = compTable[trim(trimmedLine.substr(trimmedLine.find("=") + 1, endPos - (trimmedLine.find("=") + 1)))];
-                dest = destTable[trim(trimmedLine.substr(0, trimmedLine.find("=")))];
+                comp = compTable[trim(line.substr(line.find("=") + 1, endPos - (line.find("=") + 1)))];
+
+                dest = (!dest.empty()) ? dest : "000";
                 a = comp[0];
                 binCode = "111" + a + comp.substr(1) + dest + jmp;
+
                 cout << binCode << endl;
             }
         }
